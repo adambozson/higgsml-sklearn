950a75e6e51b06f10ec91d04f6220280d1565fb3
Adam Bozson <adam.bozson@cern.ch>
2016-06-08 15:15:54 +0100

    Refactor into functions, change to subprocess for commands, add exceptions to handle missing virtualenv and keyboard interrupts.

--- setup.py
+++ setup.py
@@ -1,29 +1,55 @@
-import os, glob
+import os, glob, sys, subprocess
 
 VENV_DIRECTORY='env'
 VENV_PROMPT='higgsml'
 ACTIVATE_LINK='activate'
 REQUIREMENTS='requirements/requirements*.txt'
 
-os.system('virtualenv --prompt={} {}'.format(
-    VENV_PROMPT, VENV_DIRECTORY))
+def create_venv():
+    opts = ['virtualenv',
+            '--prompt={}'.format(VENV_PROMPT),
+            VENV_DIRECTORY]
+    try:
+        subprocess.call(opts)
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            print('Can\'t find virtualenv. Make sure virtualenv ' +
+                  'is installed before running this script.')
+            sys.exit(1)
+        else:
+            raise
 
-print('Making symbolic links...')
-try:
-    os.remove(ACTIVATE_LINK)
-    print('Replacing old link.')
-except OSError:
-    pass
-os.symlink('{}/bin/activate'.format(VENV_DIRECTORY), ACTIVATE_LINK)
+def create_link():
+    print('Making symbolic links...')
+    try:
+        os.remove(ACTIVATE_LINK)
+        print('Replacing old link.')
+    except OSError:
+        pass
+    os.symlink('{}/bin/activate'.format(VENV_DIRECTORY), ACTIVATE_LINK)
 
-print('Installing packages...')
-this_pip = '{}/bin/pip'.format(VENV_DIRECTORY)
-for requirements_file in sorted(glob.iglob(REQUIREMENTS)):
-    os.system('{} install --upgrade -r {}'.format(this_pip,
-                                        requirements_file))
-print('Installation complete.')
-print
-print('To activate the {} environment, run'.format(VENV_PROMPT))
-print('\t source {}'.format(ACTIVATE_LINK))
-print('from this directory ({}).'.format(os.getcwd()))
-print
+def install_requirements():
+    print('Installing packages...')
+    this_pip = '{}/bin/pip'.format(VENV_DIRECTORY)
+    try:
+        for requirements_file in sorted(glob.iglob(REQUIREMENTS)):
+            subprocess.call([this_pip,
+                             'install',
+                             '--upgrade',
+                             '-r', requirements_file])
+    except (KeyboardInterrupt, SystemExit):
+        raise # Allow the exception to actually stop the control flow
+    print('Installation complete.')
+
+def show_final_message():
+    print
+    print('To activate the {} environment, run'.format(VENV_PROMPT))
+    print('\t source {}'.format(ACTIVATE_LINK))
+    print('from this directory ({}).'.format(os.getcwd()))
+    print
+
+if __name__ == '__main__':
+    create_venv()
+    create_link()
+    install_requirements()
+    show_final_message()
