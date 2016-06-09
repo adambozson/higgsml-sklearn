3173735dd03117d87a311982b418c7540219c668
Adam Bozson <adam.bozson@cern.ch>
2016-06-09 17:05:52 +0100

    Add robustness against old python

--- setup.py
+++ setup.py
@@ -5,9 +5,18 @@ VENV_PROMPT = 'higgsml'
 ACTIVATE_LINK = 'activate'
 REQUIREMENTS = 'requirements/requirements*.txt'
 
+def check_python():
+    major = sys.version_info[0]
+    minor = sus.version_info[1]
+    if major < 3 and (major == 2 and minor < 7):
+        print('Requires at least Python 2.7')
+        sys.exit(1)
+
+
 def create_venv():
     opts = ['virtualenv',
             '--prompt={}'.format(VENV_PROMPT),
+            '--python={}'.format(sys.executable)
             VENV_DIRECTORY]
     try:
         subprocess.call(opts)
